e5ecbd971d02eb300f9efce750d2941956004fb4
Anas Firly <anasfirly20@gmail.com>
2023-01-01 21:09:09 +0300

    added transition delay for hire me

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
         {/* HIRE ME */}
         <div className="hidden md:flex items-center">
           <a href="https://t.me/anasfirly20" target="break">
-            <div className="text-[#2D2B2B] uppercase flex items-center gap-1 cursor-pointer border-2 rounded-md border-[#2D2B2B] p-[0.45rem] lg:px-12 hover:text-[#e1dfde] hover:bg-[#2D2B2B]  ">
+            <div className="transition ease-in-out delay-50 text-[#2D2B2B] uppercase flex items-center gap-1 cursor-pointer border-2 rounded-md border-[#2D2B2B] p-[0.45rem] lg:px-12 hover:text-[#e1dfde] hover:bg-[#2D2B2B]">
               Hire Me
               <BsTelegram />
             </div>
